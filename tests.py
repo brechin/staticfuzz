90b430b654cc8072ae92fe75239ad1a6a09a8396
Jason Brechin <Jason@ave81.com>
2016-02-04 20:10:36 -0600

    Add boilerplate to allow tests to run with GAE deps

--- tests.py
+++ tests.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('lib')
+sys.path.insert(0, "/usr/local/google_appengine")
+import dev_appserver
+dev_appserver.fix_sys_path()
 import pytest
 import staticfuzz
 
@@ -14,6 +19,6 @@ def test_index_route(client):
 
 
 def test_new_memory(client):
-    resp = client.post('/new_memory', data={'text': u'/danbooru goo_girl'},
+    resp = client.post('/new_memory', data={'text': '/danbooru goo_girl'},
                        follow_redirects=True)
     assert resp.status_code == 200
